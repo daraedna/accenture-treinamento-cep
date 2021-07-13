3168bd599b85807c2b36da5da70446482f20a817
Daraédna Menezes <daraedna@gmail.com>
2021-07-13 20:21:48 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  state = {
+    cep: {
+
+    }
+  }
+  handleTextChange(e){
+    fetch(`https://viacep.com.br/ws/${e.target.value}/json`)
+      .then(res => res.json())
+      .then(res => {
+        this.setState({
+          cep: res
+        })
+      })
+  }
+  render(){
+    console.log(this.state.cep)
+    return(
+      <div className="app">
+          <h1>Buscar CEP</h1>
+          <strong>Olá, {this.props.name}</strong>
+          <div>
+              Digite o cep:
+              <input className="input" type="text" onChange={this.handleTextChange.bind(this)} />
+          </div>
+          <div className="data-cep">
+            {Object.entries(this.state.cep).map((values, key) => {
+              return(
+                <p key={key}><strong>{values[0]}:</strong>{values[1]}</p>
+              )
+            })}
+          </div>
+      </div>
+    )
+  }
 }
 
 export default App;
