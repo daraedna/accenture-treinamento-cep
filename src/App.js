e95db2176bfc724a330c722510f375259889b2ea
Daraédna Menezes <daraedna@gmail.com>
2021-07-18 20:32:27 -0300

    add styled-components

--- src/App.js
+++ src/App.js
@@ -1,32 +1,79 @@
 import React, { useState } from 'react';
-import './App.css';
+import { s } from './styles';
+
 
 function App(props){
-  const [cep, setCep] = useState({});
+  const [cep, setCep] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [address, setAddress] = useState({});
 
-  function handleTextChange(e){
-    e.preventDefault()
-    fetch(`https://viacep.com.br/ws/${e.target.value}/json`)
-      .then(res => res.json())
-      .then(res => setCep(res))
-  }
+  function handleCep(){
+    setAddress({})
+    setLoading(true);
+      fetch(`https://viacep.com.br/ws/${cep}/json`)
+        .then(res => res.json())
+        .then(res => {
+          if(res.erro) {
+            alert("Não foi possível encontrar o CEP");
+            setLoading(false);
+          } else {
+            setAddress(res)
+            setLoading(false);
+          }
+        });
+    }
 
   return(
-    <div className="app">
-        <h1>Buscar CEP</h1>
-        <strong>Olá, {props.name}</strong>
-        <div>
-            Digite o cep:
-            <input className="input" type="text" onChange={handleTextChange} />
-        </div>
-        <div className="data-cep">
-          {Object.entries(cep).map((values, key) => {
-            return(
-              <p key={key}><strong>{values[0]}:</strong>{values[1]}</p>
-            )
-          })}
-        </div>
-    </div>
+    <s.Container>
+        <s.Title>Buscar CEP</s.Title>
+        <s.ContentSearch>
+            <s.InputSearch
+              type="number"
+              onChange={(e) => setCep(e.target.value)}
+              placeholder="Insira o CEP"
+            />
+            <s.Button
+              onClick={handleCep}
+              primary={cep.length === 8}
+              disabled={cep.length !== 8}
+            >
+              {loading ? <s.Loading />
+              : 'Pesquisar'
+              }
+            </s.Button>
+        </s.ContentSearch>
+
+          {!loading && Object.values(address).length !== 0 &&
+            <s.ContentAddress>
+              <s.AreaItem>
+                <s.ItemAddress>
+                  <s.Label>Cidade</s.Label>
+                  <s.InfoAddress width={350}>{address.localidade}</s.InfoAddress>
+                </s.ItemAddress>
+                <s.ItemAddress>
+                  <s.Label>Endereço</s.Label>
+                  <s.InfoAddress width={350}>{address.logradouro}</s.InfoAddress>
+                </s.ItemAddress>
+                <s.ItemAddress>
+                  <s.Label>Complemento</s.Label>
+                  <s.InfoAddress width={350}>{address.complemento}</s.InfoAddress>
+                </s.ItemAddress>
+              </s.AreaItem>
+
+              <s.AreaItem>
+                <s.ItemAddress>
+                  <s.Label>Bairro</s.Label>
+                  <s.InfoAddress width={200}>{address.bairro}</s.InfoAddress>
+                </s.ItemAddress>
+                <s.ItemAddress>
+                  <s.Label>UF</s.Label>
+                  <s.InfoAddress width={115}>{address.uf}</s.InfoAddress>
+                </s.ItemAddress>
+              </s.AreaItem>
+
+            </s.ContentAddress>
+          }
+    </s.Container>
   )
 }
 
